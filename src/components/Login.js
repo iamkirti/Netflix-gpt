b2e91adf8ab9a724331d0a221934eca96c280557
ksahai <ksahai@opentext.com>
2024-12-18 11:54:14 +0530

    sign in sigun up form

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,23 +1,55 @@
-import React from 'react';
-import Header from './Header';
+import React, { useState } from "react";
+import Header from "./Header";
 
 const Login = () => {
+  const [isSignInForm, setISSignInForm] = useState(true);
+
+  const toggleSignInForm = () => {
+    setISSignInForm(!isSignInForm);
+  };
   return (
     <div>
-        <Header/>
-    <div className='absolute'>
-        <img src="https://assets.nflxext.com/ffe/siteui/vlv3/729ce5c2-d831-436a-8c9d-f38fea0b99b3/web/IN-en-20241209-TRIFECTA-perspective_4aef76eb-7d5b-4be0-93c0-5f67320fd878_large.jpg"
-        alt="background-image"/>
-    </div>
-    <form className='relative p-12 bg-black'>
-    <input type="text" placeholder='Email Address' className='p-2 m-2'/>
-    <input type="password" placeholder='Password' className='p-2 m-2'/>
-    <button className='p-4 m-4'>Sign In</button>
+      <Header />
+      <div className="absolute">
+        <img
+          src="https://assets.nflxext.com/ffe/siteui/vlv3/729ce5c2-d831-436a-8c9d-f38fea0b99b3/web/IN-en-20241209-TRIFECTA-perspective_4aef76eb-7d5b-4be0-93c0-5f67320fd878_large.jpg"
+          alt="background-image"
+        />
+      </div>
+      <form className="w-3/12 absolute my-36 mx-auto right-0 left-0 p-12 bg-black text-white rounded-lg bg-opacity-80">
+        <h1 className="font-bold text-3xl py-4">
+          {isSignInForm ? "Sign In" : "Sign Up"}
+        </h1>
+        <input
+          type="text"
+          placeholder="Email Address"
+          className="p-4 my-4 w-full bg-gray-700"
+        />
+        {!isSignInForm && (
+          <input
+            type="text"
+            placeholder="Full Name"
+            className="p-4 my-4 w-full bg-gray-700"
+          />
+        )}
+        <input
+          type="password"
+          placeholder="Password"
+          className="p-4 my-4 w-full  bg-gray-700"
+        />
+        <button className="p-4 my-4 bg-red-700 w-full rounded-lg">
+          {isSignInForm ? "Sign In" : "Sign Up"}
+        </button>
 
-    </form>
-    
+        <p className="p-4 cursor-pointer" onClick={toggleSignInForm}>
+          {isSignInForm
+            ? "New to Netflix? Sign Up Now"
+            : "Already Registered? Sign In Now"}
+        
+        </p>
+      </form>
     </div>
-  )
-}
+  );
+};
 
-export default Login
+export default Login;
