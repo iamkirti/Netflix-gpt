a7d60e2fdb86e66914189917f7df67dc5d49c3df
ksahai <ksahai@opentext.com>
2025-02-26 17:07:06 +0530

    added mobie api and stored responsed in redux store

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,3 +1,11 @@
 export const LOGO="https://help.nflxext.com/helpcenter/OneTrust/oneTrust_production/consent/87b6a5c0-0104-4e96-a291-092c11350111/01938dc4-59b3-7bbc-b635-c4131030e85f/logos/dd6b162f-1a32-456a-9cfe-897231c7763c/4345ea78-053c-46d2-b11e-09adaef973dc/Netflix_Logo_PMS.png";
 
 export const USER_AVATAR="https://occ-0-6247-2164.1.nflxso.net/dnm/api/v6/K6hjPJd6cR6FpVELC5Pd6ovHRSk/AAAABdpkabKqQAxyWzo6QW_ZnPz1IZLqlmNfK-t4L1VIeV1DY00JhLo_LMVFp936keDxj-V5UELAVJrU--iUUY2MaDxQSSO-0qw.png?r=e6e";
+
+export const API_OPTIONS= {
+    method: 'GET',
+    headers: {
+      accept: 'application/json',
+      Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJhZjk1YTUyNjhlOGNmMGE4YThhMjAyNjczMDY2ZWIyMSIsIm5iZiI6MTc0MDU2MDI5MC4wNzMsInN1YiI6IjY3YmVkN2EyMzljYjc4MjBkMGVmYTlkNSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.qmUTRdxHJBCMWJRRaL-TlIAi3UOXW_t5X_wlOMtXFcM'
+    }
+  };
