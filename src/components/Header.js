38b185c3b8c14812bd3df4aad0489048d52555ef
ksahai <ksahai@opentext.com>
2025-02-26 08:19:45 +0530

    fix:redirection bug

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,37 @@
-import React from 'react';
-import { signOut } from "firebase/auth";
+import React,{useEffect} from 'react';
+import { signOut,onAuthStateChanged } from "firebase/auth";
 import {auth} from "../utils/firebase";
 import { useNavigate } from 'react-router-dom';
 import { useSelector } from 'react-redux';
+import { addUser,removeUser } from '../utils/userSlice';
+import { useDispatch } from 'react-redux';
 
 
 const Header = () => {
   const navigate=useNavigate();
+   const dispatch=useDispatch();
   const user=useSelector((store)=>store.user);
+
+  useEffect(()=>{
+    onAuthStateChanged(auth, (user) => {
+      if (user) {
+        // User is signed in, see docs for a list of available properties
+        // https://firebase.google.com/docs/reference/js/auth.user
+        const {uid,email,displayName,photoURL} = user;
+        dispatch(addUser({uid:uid,email:email,displayName:displayName,photoURL:photoURL}));
+       navigate('/browse');
+        // ...
+      } else {
+        // User is signed out
+        // ...
+        dispatch(removeUser());
+        navigate('/');
+       
+      }
+    });
+    
+
+  },[])
   const handleSignout=()=>{
    
 signOut(auth).then(() => {
--- src/components/Login.js
+++ src/components/Login.js
@@ -3,12 +3,11 @@ import Header from "./Header";
 import { checkValidateData } from "../utils/validate";
 import {  createUserWithEmailAndPassword,signInWithEmailAndPassword,updateProfile } from "firebase/auth";
 import {auth} from "../utils/firebase";
-import { useNavigate } from 'react-router-dom';
 import { useDispatch } from "react-redux";
 import { addUser } from "../utils/userSlice";
 
 const Login = () => {
-  const navigate=useNavigate();
+ 
   const dispatch=useDispatch();
   const [isSignInForm, setIsSignInForm] = useState(true);
 
@@ -34,7 +33,7 @@ const name=useRef(null);
     }).then(() => {
        const {uid,email,displayName,photoURL} = auth.currentUser;
                         dispatch(addUser({uid:uid,email:email,displayName:displayName,photoURL:photoURL}));
-      navigate("/browse");
+      
       // Profile updated!
       // ...
     }).catch((error) => {
@@ -60,8 +59,8 @@ const name=useRef(null);
   .then((userCredential) => {
     // Signed in 
     const user = userCredential.user;
-    console.log(user);
-    navigate("/browse");
+    
+    
   })
   .catch((error) => {
     const errorCode = error.code;
