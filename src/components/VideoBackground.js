9a598319ff6603bf7ddc24a1e6a39a3d370d481a
ksahai <ksahai@opentext.com>
2025-03-19 19:04:52 +0530

    added the movie trailer to redux store

--- src/components/VideoBackground.js
+++ src/components/VideoBackground.js
@@ -1,9 +1,12 @@
 import React, { useEffect ,useState} from 'react';
 import { API_OPTIONS } from '../utils/constants';
+import { useDispatch, useSelector } from 'react-redux';
+import { addTrailerVideo } from '../utils/movieSlice';
 
 const VideoBackground = ({movieId}) => {
-
-  const [trailerId,setTrailerId]=useState(null);
+  const trailerVideo=useSelector(store=>store.movies?.trailerVideo);
+const dispatch=useDispatch();
+ // const [trailerId,setTrailerId]=useState(null);
 
   const getMovieVideos=async ()=>{
 
@@ -15,15 +18,17 @@ const VideoBackground = ({movieId}) => {
 
     const trail=filterdata.length?filterdata[0]:json.results[0];
     console.log(trail);
-    setTrailerId(trail.key)
+   // setTrailerId(trail.key)
+dispatch(addTrailerVideo(trail));
 
+console.log("trailerVideo",trailerVideo)
   }
 
   useEffect(()=>{
 getMovieVideos();
   },[])
   return (
-    <div><iframe width="560" height="315" src={"https://www.youtube.com/embed/"+trailerId} title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerPolicy="strict-origin-when-cross-origin" allowFullScreen></iframe></div>
+    <div><iframe width="560" height="315" src={"https://www.youtube.com/embed/"+trailerVideo?.key} title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerPolicy="strict-origin-when-cross-origin" allowFullScreen></iframe></div>
   )
 }
 
